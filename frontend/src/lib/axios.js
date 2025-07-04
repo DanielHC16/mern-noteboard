451d163666c47b6acff5c19a9bc5a37e44c0f907
Daniel Camacho <danielcamacho0416@gmail.com>
2025-07-04 16:35:07 +0800

    Minor fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// in producttion, there's no local host -- we have to make the BASE_URL dynamic (depending on the domain we deploy it to)
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+// in production, there's no local host -- we have to make the BASE_URL dynamic (depending on the domain we deploy it to)
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
 
 const api = axios.create({
     baseURL: BASE_URL 
-})
+});
 
 export default api;
