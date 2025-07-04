9fba63b739d590234db64917a1821498846978e5
Daniel Camacho <danielcamacho0416@gmail.com>
2025-07-04 16:21:17 +0800

    Configured for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "../config/db.js";
@@ -13,15 +13,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
-app.use(cors({
-  origin: "http://localhost:5173", // Allow requests from the frontend
-  })
-); 
+
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin: "http://localhost:5173", // Allow requests from the frontend
+    })
+  ); 
+}
 
 app.use(rateLimiter); // Apply rate limiting middleware
 
@@ -38,6 +42,15 @@ app.use((req, res, next) => { // Middleware to log request details
 app.use("/api/notes", notesRoutes); // linking the notes routes to the /api/notes endpoint
 // For update: change routes and create a new route.js to handle different routing.
 // Ex: app.use("/api/product", productsRoutes);
+
+if(process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*",(req, res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
+
 
 connectDB().then(() => { // Once the database is connected, start the server
   app.listen(PORT, () => {
